beac3d6ff2c475625b64889753470b342e0f359b
pie0902 <zhfoqkq@gmail.com>
2024-05-21 10:18:50 +0900

    git init

---
+++ src/main/java/org/example/corppractice/CorpPracticeApplication.java
@@ -0,0 +1,13 @@
+package org.example.corppractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorpPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorpPracticeApplication.class, args);
+    }
+
+}
